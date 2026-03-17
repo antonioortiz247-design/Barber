d5083d7de746fabd15d9eaa5877eb7a6154ca412
PATTS <antonioortiz247@gmail.com>
2026-03-16 20:57:23 -0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}","./components/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: { colors: { primary:"#000", accent:"#C9A227" } } },
+  plugins: []
+};
